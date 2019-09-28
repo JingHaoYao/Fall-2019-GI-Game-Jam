285a7e25477ae636996dfe7011516eba8a120e34
Harry Hu <thefinals@v1040-wn-rt-a-120-14.campus-dynamic.uwaterloo.ca>
2019-09-28 14:21:14 -0400

    started on turrets

---
+++ GI Game Jam Fall 2019/Assets/Scripts/Turret.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float ReloadTime; //Reference values, usually constant
+    public float RangeValue;
+    public int BulletDamage;
+    public GameObject BulletType;
+    public float BulletSpeed;
+    CircleCollider2D Range;
+
+    public float Reloading; //Active variables
+    public GameObject Bullet;
+    public int Quadrant;
+    public int LowestTarget = 9999;
+
+    GameObject currentTarget;
+
+    private void Start()
+    {
+        Range = GetComponent<CircleCollider2D>();
+        Range.radius = RangeValue;
+    }
+
+    private void Update()
+    {
+        if (currentTarget)
+        {
+            float angle = Mathf.Atan2(currentTarget.transform.position.y - transform.position.y, currentTarget.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+            if (Reloading <= 0)
+            {
+                Bullet = Instantiate(BulletType);
+                Bullet.transform.rotation = transform.rotation;
+                Bullet.GetComponent<TowerProjectile>().damage = BulletDamage;
+                Bullet.GetComponent<TowerProjectile>().speed = BulletSpeed;
+                Bullet.GetComponent<TowerProjectile>().Direction = Quadrant;
+                Bullet.transform.position = transform.position;
+                Reloading = ReloadTime;
+            }
+            else
+            {
+                Reloading -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D Target)
+    {
+        if (Target.gameObject.GetComponent<Enemy>())
+        {
+            if (currentTarget.gameObject.GetComponent<Enemy>().Order <= LowestTarget)
+            {
+                currentTarget.gameObject.GetComponent<Enemy>().Order = LowestTarget;
+            }
+            if (currentTarget.gameObject.GetComponent<Enemy>().Order == LowestTarget)
+            {
+                currentTarget = Target.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject == currentTarget)
+        {
+            currentTarget = null;
+            LowestTarget = 9999;
+        }
+    } 
+}
